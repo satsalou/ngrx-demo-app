643190a2249cf226caabccd6dd5eecdb67813198
Sofia Atsalou <sofia.atsalou@gmail.com>
2018-09-26 19:04:04 +0300

    refactor(counter-reducer) Return a deep copy of the state

--- src/app/reducers/counter.reducer.ts
+++ src/app/reducers/counter.reducer.ts
@@ -1,24 +1,19 @@
 import { Action } from '@ngrx/store'
 import * as CounterActions from './../actions/counter.actions'
+import { AppState } from '../models/app-state.model';
 
 const initialAppState = {
   counter: 0
 }
 
-export function reducer(appState = initialAppState, action: CounterActions.Actions) {
+export function reducer(appState = initialAppState, action: CounterActions.Actions):AppState {
   switch (action.type) {
     case CounterActions.INCREMENT:
       appState.counter = appState.counter + 1;
-      return {
-        counter: appState.counter,
-        payload: action.payload
-      }
+      return Object.assign({ payload: action.payload }, appState);
     case CounterActions.DECREMENT:
       appState.counter = appState.counter - 1;
-      return {
-        counter: appState.counter,
-        payload: action.payload
-      };
+      return Object.assign({ payload: action.payload }, appState);
     default:
       return appState;
   }
